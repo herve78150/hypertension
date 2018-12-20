6807f2cf8c74763236dd951e2b0bfdd0aee41a0c
Hervé Debry <herve.debry@free.fr>
2018-12-20 13:01:27 +0100

    Update loadjson.async.js

--- javascript/loadjson.async.js
+++ javascript/loadjson.async.js
@@ -25,7 +25,7 @@ async function loadjson(fichier) {
       google.charts.load("current", { packages: ["corechart", "line"] });
       google.charts.setOnLoadCallback(drawChart);
     }else{
-      console.log(response.status);
+      console.log(response);
       $titre.innerHTML = `<h3>${fichier}: error ${response.status}= ${response.statusText}</h3>`;
       $table.innerHTML = '';
       $graph.innerHTML = '';
